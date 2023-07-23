660f6ee9347190bdde4b8873ae618b9f1b35cea3
Nikiths1 <nikith.s1@mphasis.com>
2023-07-23 15:15:14 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BasicCSharpAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer to calculate its factorial:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            long factorial = CalculateFactorial(number);
+
+            Console.WriteLine($"Factorial of {number} is {factorial}");
+        }
+
+        static long CalculateFactorial(int n)
+        {
+            if (n == 0)
+                return 1;
+
+            return n * CalculateFactorial(n - 1);
+        }
+    }
+}
